3fedf0eb6c96435149fef65886fbe5b05c0eddf1
Artur Das <a.daschevici@gmail.com>
2024-12-03 18:49:48 +0100

    Add the bonus in the null challenge

--- src/challenges/challenge0.rs
+++ src/challenges/challenge0.rs
@@ -1,9 +1,28 @@
-use axum::{routing::get, Router};
+use axum::{
+    http::{header, StatusCode},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 
 async fn hello_bird() -> &'static str {
     "Hello, bird!"
 }
 
+async fn redirect_to() -> Response {
+    (
+        StatusCode::FOUND,
+        [(
+            header::LOCATION,
+            "https://www.youtube.com/watch?v=9Gc4QTqslN4",
+        )],
+        (), // No response body
+    )
+        .into_response()
+}
+
 pub fn router() -> Router {
-    Router::new().route("/-1", get(hello_bird))
+    Router::new()
+        .route("/-1", get(hello_bird))
+        .route("/-1/seek", get(redirect_to))
 }
