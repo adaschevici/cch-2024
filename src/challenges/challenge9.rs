e0cb725e4c4320e47e28d49a408095622ae36f63
Artur Das <a.daschevici@gmail.com>
2024-12-11 20:51:22 +0100

    Add some conversion code to pints and different measurements

--- src/challenges/challenge9.rs
+++ src/challenges/challenge9.rs
@@ -1,5 +1,6 @@
+use axum::RequestExt;
 use axum::{
-    extract::State,
+    extract::{Request, State},
     http::{HeaderMap, StatusCode},
     response::{IntoResponse, Response, Result},
     routing::post,
@@ -7,6 +8,7 @@ use axum::{
 };
 use leaky_bucket::RateLimiter;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::{sync::Arc, time::Duration};
 use thiserror::Error;
 use tokio::sync::Mutex;
@@ -41,17 +43,45 @@ impl IntoResponse for AppError {
     }
 }
 
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Unit {
+    Liters(f64),
+    Gallons(f64),
+    Pints(f64),
+}
+
 async fn get_milk(
     State(bucket): State<MilkBucket>,
     headers: HeaderMap,
+    req: Request,
 ) -> Result<String, AppError> {
     let got_milk = bucket.lock().await.try_acquire(1);
     if !got_milk {
         return Err(AppError::TooManyRequests);
     }
-    // let content_type = headers
-    //     .get(axum::http::header::CONTENT_TYPE)
-    //     .ok_or(AppError::MissingContentType)?;
+    let content_type = headers.get(axum::http::header::CONTENT_TYPE);
+    if content_type == Some(&"application/json".parse().unwrap()) {
+        let Json(payload) = match req.extract::<Json<Unit>, _>().await {
+            Ok(payload) => payload,
+            Err(e) => {
+                println!("Failed to parse JSON: {}", e);
+
+                return Err(AppError::MissingContentType);
+            }
+        };
+        match payload {
+            Unit::Liters(payload) => {
+                return Ok(json!({"gallons": payload * 0.264172}).to_string());
+            }
+            Unit::Gallons(payload) => {
+                return Ok(json!({"liters": payload * 3.78541}).to_string());
+            }
+            Unit::Pints(payload) => {
+                return Ok(json!({"liters": payload * 0.473176}).to_string());
+            }
+        }
+    }
     Ok("Milk withdrawn\n".to_string())
 }
 
