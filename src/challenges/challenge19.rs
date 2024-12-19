acce7fe073b8650fb5b1075f27122887d2641a44
Artur Das <a.daschevici@gmail.com>
2024-12-19 23:47:07 +0100

    Set up all the db operations

--- src/challenges/challenge19.rs
+++ src/challenges/challenge19.rs
@@ -2,7 +2,7 @@ use axum::{
     extract::{Path, State},
     http::StatusCode,
     response::{IntoResponse, Response, Result},
-    routing::{get, post},
+    routing::{delete, get, post, put},
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
@@ -10,6 +10,7 @@ use serde_json::json;
 use sqlx::{FromRow, PgPool, Row};
 use std::sync::Arc;
 use thiserror::Error;
+use uuid::Uuid;
 
 #[derive(Debug, Clone)]
 struct AppState {
@@ -72,10 +73,27 @@ async fn reset_db(State(state): State<Arc<AppState>>) -> Result<StatusCode, AppE
 
 #[derive(FromRow, Serialize, Deserialize, Debug)]
 struct Quote {
-    id: String,
     author: String,
     quote: String,
 }
+
+#[derive(FromRow, Serialize, Deserialize, Debug)]
+struct QuoteRecord {
+    id: Uuid,
+    author: String,
+    quote: String,
+    version: i32,
+    created_at: chrono::DateTime<chrono::Utc>,
+}
+// impl Quote {
+//     fn new(author: String, quote: String) -> Self {
+//         Self {
+//             id: Uuid::new_v4(),
+//             author,
+//             quote,
+//         }
+//     }
+// }
 
 // async fn get_quote_by_id(
 //     Path(id): Path<String>,
@@ -91,12 +109,12 @@ struct Quote {
 async fn get_quote_by_id(
     State(state): State<Arc<AppState>>,
     Path(id): Path<String>,
-) -> Result<Json<Quote>, AppError> {
-    let id: i32 = id
+) -> Result<Json<QuoteRecord>, AppError> {
+    let id: Uuid = id
         .parse()
         .map_err(|_| AppError::InvalidID("Invalid ID".to_string()))?;
 
-    let quote = sqlx::query_as::<_, Quote>("SELECT * FROM quotes WHERE id = $1")
+    let quote = sqlx::query_as::<_, QuoteRecord>("SELECT * FROM quotes WHERE id = $1")
         .bind(id)
         .fetch_one(&state.pool)
         .await
@@ -108,10 +126,69 @@ async fn get_quote_by_id(
     Ok(Json(quote)) // wtf ???
 }
 
+async fn delete_quote_by_id(
+    State(state): State<Arc<AppState>>,
+    Path(id): Path<String>,
+) -> Result<Json<QuoteRecord>, AppError> {
+    let id: Uuid = id
+        .parse()
+        .map_err(|_| AppError::InvalidID("Invalid ID".to_string()))?;
+    let quote = sqlx::query_as::<_, QuoteRecord>("DELETE FROM quotes WHERE id = $1 RETURNING *")
+        .bind(id)
+        .fetch_one(&state.pool)
+        .await
+        .map_err(|e| match e {
+            sqlx::Error::RowNotFound => AppError::QuoteNotFound,
+            _ => AppError::DatabaseError(e),
+        })?;
+    Ok(Json(quote)) // wtf ???
+}
+
+async fn update_quote_by_id_increment_version(
+    State(state): State<Arc<AppState>>,
+    Path(id): Path<String>,
+) -> Result<Json<QuoteRecord>, AppError> {
+    let id: Uuid = id
+        .parse()
+        .map_err(|_| AppError::InvalidID("Invalid ID".to_string()))?;
+    let quote = sqlx::query_as::<_, QuoteRecord>(
+        "UPDATE quotes SET version = version + 1 WHERE id = $1 RETURNING *",
+    )
+    .bind(id)
+    .fetch_one(&state.pool)
+    .await
+    .map_err(|e| match e {
+        sqlx::Error::RowNotFound => AppError::QuoteNotFound,
+        _ => AppError::DatabaseError(e),
+    })?;
+    Ok(Json(quote)) // wtf ???
+}
+
+async fn add_quote_with_random_uuid_id(
+    State(state): State<Arc<AppState>>,
+    Json(quote): Json<Quote>,
+) -> Result<(StatusCode, Json<QuoteRecord>), AppError> {
+    let new_id = Uuid::new_v4();
+    let quote = sqlx::query_as::<_, QuoteRecord>(
+        "INSERT INTO quotes (id, author, quote) VALUES ($1, $2, $3) RETURNING *",
+    )
+    .bind(new_id)
+    .bind(quote.author)
+    .bind(quote.quote)
+    .fetch_one(&state.pool)
+    .await
+    .map_err(AppError::DatabaseError)?;
+    println!("quote: {:?}", quote);
+    Ok((StatusCode::CREATED, Json(quote))) // wtf ???
+}
+
 pub fn router(pool: PgPool) -> Router {
     let shared_state = Arc::new(AppState { pool });
     Router::new()
         .route("/reset", post(reset_db))
         .route("/cite/:id", get(get_quote_by_id))
+        .route("/remove/:id", delete(delete_quote_by_id))
+        .route("/undo/:id", put(update_quote_by_id_increment_version))
+        .route("/draft", post(add_quote_with_random_uuid_id))
         .with_state(shared_state)
 }
