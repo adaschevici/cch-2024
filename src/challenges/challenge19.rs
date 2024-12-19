dba4744ea2126902a3ad9cf2cc5c89bec934302d
Artur Das <a.daschevici@gmail.com>
2024-12-19 20:22:37 +0100

    Initialize db using migrations i think this might be a better approach

--- src/challenges/challenge19.rs
+++ src/challenges/challenge19.rs
@@ -37,18 +37,18 @@ async fn reset_db(State(state): State<Arc<AppState>>) -> Result<StatusCode, AppE
         .await
         .unwrap();
 
-    sqlx::query(
-        "CREATE TABLE IF NOT EXISTS quotes (
-        id UUID PRIMARY KEY,
-        author TEXT NOT NULL,
-        quote TEXT NOT NULL,
-        created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        version INT NOT NULL DEFAULT 1
-    );",
-    )
-    .execute(&state.pool)
-    .await
-    .unwrap();
+    // sqlx::query(
+    //     "CREATE TABLE IF NOT EXISTS quotes (
+    //     id UUID PRIMARY KEY,
+    //     author TEXT NOT NULL,
+    //     quote TEXT NOT NULL,
+    //     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    //     version INT NOT NULL DEFAULT 1
+    // );",
+    // )
+    // .execute(&state.pool)
+    // .await
+    // .unwrap();
 
     Ok(StatusCode::OK)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ mod challenges;
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .expect("Failed to run migrations");
     let router = Router::new().nest("/", challenges::router(pool.clone()));
 
     Ok(router.into())
